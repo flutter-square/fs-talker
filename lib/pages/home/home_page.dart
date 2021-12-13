40f8b2c3656947437afc9980a0928faa03ff5fae
taitai9847 <taitai9847@gmail.com>
2021-12-13 13:19:47 +0900

    add: homeページの追加

---
+++ lib/pages/home/home_page.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:fs_talker/components/custom_app_bar.dart';
+import 'package:fs_talker/components/custom_bottom_navigation_bar.dart';
+
+import '../../constants.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(
+        title: 'Home',
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(Styles.size32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Flexible(
+              flex: 8,
+              child: Center(
+                child: Image.asset(
+                  "assets/images/home_pic.png",
+                ),
+              ),
+            ),
+            Flexible(
+              child: SizedBox(
+                height: Styles.size48,
+                child: OutlinedButton(
+                  onPressed: () {
+                    ///遷移処理
+                  },
+                  child: Text(
+                    "roomに参加する",
+                    style: Styles.buttonLabelStyle.copyWith(
+                      color: Styles.blackLabelColor
+                    ),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    elevation: 0.0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(Styles.size14),
+                    ),
+                    side: BorderSide(
+                      width: 0.5,
+                      color: Styles.primaryColor,
+                    )
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: Styles.size16,
+            ),
+            Flexible(
+              child: SizedBox(
+                height: Styles.size48,
+                child: ElevatedButton(
+                  onPressed: () {
+                    ///遷移処理
+                  },
+                  child: Text(
+                    "roomを作成する",
+                    style: Styles.buttonLabelStyle.copyWith(
+                        color: Styles.whiteLabelColor
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      elevation: 0.0,
+                      primary: Styles.primaryColor,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(Styles.size14)
+                      )
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: CustomBottomNavigationBar(),
+    );
+  }
+}
