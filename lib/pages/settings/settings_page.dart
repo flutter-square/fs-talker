b5c589c79983b4cd1c8702136437f5655af604ca
taitai9847 <taitai9847@gmail.com>
2021-12-14 16:04:59 +0900

    add: settingページの追加

--- lib/pages/settings/settings_page.dart
+++ lib/pages/settings/settings_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fs_talker/components/custom_app_bar.dart';
 
+import '../../constants.dart';
+
 class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
 
@@ -9,6 +11,15 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+
+  List<String> settings  = [
+    "お問い合わせ",
+    "プライバシーポリシー",
+    "利用規約",
+    "ライセンス",
+    "バージョン",
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,8 +27,102 @@ class _SettingsState extends State<Settings> {
         title: "settings",
       ),
       body: Padding(
-        padding: EdgeInsets.all(8),
-        child: Text("settings"),
+        padding: EdgeInsets.fromLTRB(24, 32, 24, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+              ),
+              height: 64,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border.all(
+                              color: Colors.black,
+                              width: 0.1
+                          ),
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              fit: BoxFit.fill,
+                              image: AssetImage("assets/images/home_pic.png")
+                          )
+                      ),
+                    ),
+                    SizedBox(width: 8,),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Taiyo Ishikawa",
+                          style: Styles.profileNameLabelStyle.copyWith(
+                            fontSize: 16
+                          ),
+                        ),
+                        Text(
+                          "@taitai",
+                          style: TextStyle(
+                              color: Styles.greyLabelColor
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 32,),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                ),
+                child: ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (context, index) {
+                    final settingLabel = settings[index];
+                    return GestureDetector(
+                      onTap: () {
+                        print("tapped");
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border(bottom: BorderSide(width: 0.2, color: Colors.grey))
+                        ),
+                        height: 64,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(settingLabel),
+                              Icon(
+                                Icons.arrow_forward_ios,
+                                color: Colors.grey,
+                                size: 16,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: settings.length,
+                ),
+              )
+            )
+          ],
+        ),
       ),
     );
   }
